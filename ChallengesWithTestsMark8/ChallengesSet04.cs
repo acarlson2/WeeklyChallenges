595eb182a6088a9aff2fe2b6edd0705212c1e716
Aaron Carlson <acarlson2@carthage.edu>
2024-07-08 11:52:01 -0500

    set 4 complete

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +7,134 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int retVal = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    retVal += numbers[i];
+                }
+                else
+                {
+                    retVal -= numbers[i];
+                }
+            }
+
+            return retVal;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            string[] strings = new string[] { str1, str2, str3, str4 };
+            int retVal = str1.Length;
+
+            for(int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length < retVal)
+                {
+                    retVal = strings[i].Length;
+                }
+            }
+
+            return retVal;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] ints = new int[] { number1, number2, number3, number4 };
+            int retVal = number1;
+
+            for(int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] < retVal)
+                {
+                    retVal = ints[i];
+                }
+            }
+
+            return retVal;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if(sideLength1 + sideLength2 > sideLength3 && sideLength2 + sideLength3 > sideLength1 &&
+                sideLength1 + sideLength3 > sideLength2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            bool retVal = double.TryParse(input, out double a);
+            return retVal;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int nullCount = 0;
+            for(int i = 0; i < objs.Length; i++)
+            {
+                if (objs[i] == null)
+                {
+                    nullCount++;
+                }
+            }
+
+            if(objs.Length/2 < nullCount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            var retVal = numbers.Where(num => num % 2 == 0);
+
+            if(retVal.Count() == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return retVal.Average();
+            }
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if(number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int result = 1;
+
+            for(int i = number; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            return result;
         }
     }
 }
